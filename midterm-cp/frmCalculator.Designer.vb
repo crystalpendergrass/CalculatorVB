704a94cc1c9d39600e55a4f963e0416e18ca0094
Crystal <crystalpendergrass@users.noreply.github.com>
2015-07-22 09:58:15 -0400

    initial commit

---
+++ midterm-cp/frmCalculator.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.btnZero = New System.Windows.Forms.Button()
+        Me.btnMinus = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.btnDecimal = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnOne
+        '
+        Me.btnOne.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOne.Location = New System.Drawing.Point(17, 196)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(30, 30)
+        Me.btnOne.TabIndex = 1
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTwo.Location = New System.Drawing.Point(57, 196)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(30, 30)
+        Me.btnTwo.TabIndex = 2
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnThree.Location = New System.Drawing.Point(97, 196)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(30, 30)
+        Me.btnThree.TabIndex = 3
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFour.Location = New System.Drawing.Point(17, 161)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(30, 30)
+        Me.btnFour.TabIndex = 4
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFive.Location = New System.Drawing.Point(57, 161)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(30, 30)
+        Me.btnFive.TabIndex = 5
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSix.Location = New System.Drawing.Point(97, 161)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(30, 30)
+        Me.btnSix.TabIndex = 6
+        Me.btnSix.Text = "6"
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEight.Location = New System.Drawing.Point(57, 126)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(30, 30)
+        Me.btnEight.TabIndex = 8
+        Me.btnEight.Text = "8"
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNine.Location = New System.Drawing.Point(97, 126)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(30, 30)
+        Me.btnNine.TabIndex = 9
+        Me.btnNine.Text = "9"
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'btnZero
+        '
+        Me.btnZero.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnZero.Location = New System.Drawing.Point(17, 232)
+        Me.btnZero.Name = "btnZero"
+        Me.btnZero.Size = New System.Drawing.Size(70, 30)
+        Me.btnZero.TabIndex = 10
+        Me.btnZero.Text = "0"
+        Me.btnZero.UseVisualStyleBackColor = True
+        '
+        'btnMinus
+        '
+        Me.btnMinus.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMinus.Location = New System.Drawing.Point(153, 202)
+        Me.btnMinus.Name = "btnMinus"
+        Me.btnMinus.Size = New System.Drawing.Size(55, 25)
+        Me.btnMinus.TabIndex = 12
+        Me.btnMinus.Text = "-"
+        Me.btnMinus.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiply.Location = New System.Drawing.Point(153, 140)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(55, 25)
+        Me.btnMultiply.TabIndex = 13
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDivide.Location = New System.Drawing.Point(153, 110)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(55, 25)
+        Me.btnDivide.TabIndex = 14
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'btnEqual
+        '
+        Me.btnEqual.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEqual.Location = New System.Drawing.Point(153, 233)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(55, 25)
+        Me.btnEqual.TabIndex = 15
+        Me.btnEqual.Text = "="
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'btnDecimal
+        '
+        Me.btnDecimal.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDecimal.Location = New System.Drawing.Point(97, 232)
+        Me.btnDecimal.Name = "btnDecimal"
+        Me.btnDecimal.Size = New System.Drawing.Size(30, 30)
+        Me.btnDecimal.TabIndex = 17
+        Me.btnDecimal.Text = "."
+        Me.btnDecimal.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.SystemColors.Control
+        Me.btnClear.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(17, 90)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(110, 25)
+        Me.btnClear.TabIndex = 16
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.BackColor = System.Drawing.Color.White
+        Me.txtDisplay.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDisplay.Location = New System.Drawing.Point(17, 17)
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.ReadOnly = True
+        Me.txtDisplay.Size = New System.Drawing.Size(190, 41)
+        Me.txtDisplay.TabIndex = 18
+        Me.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnSeven
+        '
+        Me.btnSeven.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSeven.Location = New System.Drawing.Point(17, 126)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(30, 30)
+        Me.btnSeven.TabIndex = 19
+        Me.btnSeven.Text = "7"
+        Me.btnSeven.UseVisualStyleBackColor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Location = New System.Drawing.Point(153, 172)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(55, 25)
+        Me.btnPlus.TabIndex = 20
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'frmCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(234, 286)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btnSeven)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDecimal)
+        Me.Controls.Add(Me.btnEqual)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnMinus)
+        Me.Controls.Add(Me.btnZero)
+        Me.Controls.Add(Me.btnNine)
+        Me.Controls.Add(Me.btnEight)
+        Me.Controls.Add(Me.btnSix)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Name = "frmCalculator"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOne As System.Windows.Forms.Button
+    Friend WithEvents btnTwo As System.Windows.Forms.Button
+    Friend WithEvents btnThree As System.Windows.Forms.Button
+    Friend WithEvents btnFour As System.Windows.Forms.Button
+    Friend WithEvents btnFive As System.Windows.Forms.Button
+    Friend WithEvents btnSix As System.Windows.Forms.Button
+    Friend WithEvents btnEight As System.Windows.Forms.Button
+    Friend WithEvents btnNine As System.Windows.Forms.Button
+    Friend WithEvents btnZero As System.Windows.Forms.Button
+    Friend WithEvents btnMinus As System.Windows.Forms.Button
+    Friend WithEvents btnMultiply As System.Windows.Forms.Button
+    Friend WithEvents btnDivide As System.Windows.Forms.Button
+    Friend WithEvents btnEqual As System.Windows.Forms.Button
+    Friend WithEvents btnDecimal As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents txtDisplay As System.Windows.Forms.TextBox
+    Friend WithEvents btnSeven As System.Windows.Forms.Button
+    Friend WithEvents btnPlus As System.Windows.Forms.Button
+
+End Class
---
+++ midterm-cp/frmCalculator.vb
@@ -0,0 +1,337 @@
+﻿' Program name: Midterm Exam - Calculator
+' Author:       Crystal Pendergrass
+' Date:         9 March 2015
+' Purpose:      The Calculator works similar to the Windows calculator. The user 
+'               types the first number using the keypad buttons of the calculator 
+'               (no keyboard input required), presses an operation, then the user types 
+'               the second number using the keypad buttons of the calculator (no keyboard 
+'               input required). If it clicks the ‘=’ it performs the operation. If it 
+'               clicks an operation button, it performs the previous calculation, shows 
+'               the result of the first calculation and waits for the second operand of 
+'               the new operation. If the user clicks Clear, the calculator shows the 
+'               value 0 and resets its memory.
+
+Public Class frmCalculator
+
+    Dim decLeftOperand As Decimal
+    Dim decRightOperand As Decimal
+    Dim decResult As Decimal
+    Dim strOperator As String
+    Dim strLastOperator As String
+    Dim blnIsFirstDigit As Boolean
+
+    '****************************************************************************** 
+    'When the form is loaded the display is set to 0 and the flag to indicate that
+    'the next number entered is the first digit of some number.  
+    '******************************************************************************
+    Private Sub frmCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtDisplay.Text = "0"
+        blnIsFirstDigit = True
+    End Sub
+
+    '****************************************************************************** 
+    'When the "Clear" button is pressed the display is reset to "0" and any values 
+    'stored for the operands and operators are erased.  The user can now begin
+    'entering digits for a new number
+    '******************************************************************************
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDisplay.Text = "0"
+        blnIsFirstDigit = True
+        decLeftOperand = Nothing
+        decRightOperand = Nothing
+        decResult = Nothing
+        strOperator = Nothing
+
+        btnDecimal.Enabled = True
+    End Sub
+
+    '****************************************************************************** 
+    'The following subroutines control how the number buttons behave.  Because they 
+    'essentially behave the same, this comment will describe all of them.  
+    'If either operand is empty and the user is entering the first digit of a 
+    'number, then the display is set to the value of that button.  Otherwise 
+    'any pressed number will be added to the exisiting numbers in the display.
+    '******************************************************************************
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnOne.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnOne.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnTwo.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnTwo.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnThree.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnThree.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnFour.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnFour.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnFive.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnFive.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSix_Click(sender As Object, e As EventArgs) Handles btnSix.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnSix.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnSix.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSeven_Click(sender As Object, e As EventArgs) Handles btnSeven.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnSeven.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnSeven.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnEight_Click(sender As Object, e As EventArgs) Handles btnEight.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnEight.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnEight.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnNine_Click(sender As Object, e As EventArgs) Handles btnNine.Click
+        If ((decLeftOperand = Nothing Or decRightOperand = Nothing) And decResult = Nothing) Then
+            If (blnIsFirstDigit) Then
+                txtDisplay.Text = btnNine.Text
+                blnIsFirstDigit = False
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnNine.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnZero_Click(sender As Object, e As EventArgs) Handles btnZero.Click
+        If (blnIsFirstDigit) Then
+            txtDisplay.Text = btnZero.Text
+            blnIsFirstDigit = False
+        Else
+            If (txtDisplay.Text = "0") Then
+                txtDisplay.Text = btnZero.Text
+            Else
+                txtDisplay.Text = txtDisplay.Text & btnZero.Text
+            End If
+        End If
+    End Sub
+
+    '****************************************************************************** 
+    'When the decimal button is pressed it is added to the display and then the 
+    'decimal button is disabled. 
+    '******************************************************************************
+    Private Sub btnDecimal_Click(sender As Object, e As EventArgs) Handles btnDecimal.Click
+        If (txtDisplay.Text <> "0") Then
+            txtDisplay.Text = txtDisplay.Text & btnDecimal.Text
+        Else
+            txtDisplay.Text = "0" & btnDecimal.Text
+        End If
+        btnDecimal.Enabled = False
+    End Sub
+
+    '****************************************************************************** 
+    'This comment covers all operator buttons because they behave essentially the
+    'same.  When an operator button is pressed the user can enter a new number and
+    'the decimal button is enabled. If the user presses an operator in series, the
+    'first calculation is performed and its results are shown in the display.  If
+    'the user attempts to divide by zero, "ERROR" is displayed and the user must 
+    'start a new calculation (ie all values stored are erased).
+    '******************************************************************************
+    Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+        blnIsFirstDigit = True
+        btnDecimal.Enabled = True
+
+        If (decLeftOperand = Nothing And decRightOperand = Nothing) Then
+            decLeftOperand = Convert.ToDecimal(txtDisplay.Text)
+            strOperator = btnPlus.Text
+            txtDisplay.Text = Convert.ToString(decLeftOperand)
+        Else
+            strLastOperator = strOperator
+            decRightOperand = Convert.ToDecimal(txtDisplay.Text)
+
+            If (decRightOperand = 0 And strLastOperator = "/") Then
+                txtDisplay.Text = "ERROR"
+                decRightOperand = Nothing
+                decLeftOperand = Nothing
+                strOperator = Nothing
+            Else
+                decLeftOperand = PerformCalculation(decLeftOperand, decRightOperand, strLastOperator)
+                decRightOperand = Nothing
+                strOperator = btnPlus.Text
+                txtDisplay.Text = Convert.ToString(decLeftOperand)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnMinus_Click(sender As Object, e As EventArgs) Handles btnMinus.Click
+        blnIsFirstDigit = True
+        btnDecimal.Enabled = True
+
+        If (decLeftOperand = Nothing And decRightOperand = Nothing) Then
+            decLeftOperand = Convert.ToDecimal(txtDisplay.Text)
+            strOperator = btnMinus.Text
+            txtDisplay.Text = Convert.ToString(decLeftOperand)
+        Else
+            strLastOperator = strOperator
+            decRightOperand = Convert.ToDecimal(txtDisplay.Text)
+
+            If (decRightOperand = 0 And strLastOperator = "/") Then
+                txtDisplay.Text = "ERROR"
+                decRightOperand = Nothing
+                decLeftOperand = Nothing
+                strOperator = Nothing
+            Else
+                decLeftOperand = PerformCalculation(decLeftOperand, decRightOperand, strLastOperator)
+                decRightOperand = Nothing
+                strOperator = btnMinus.Text
+                txtDisplay.Text = Convert.ToString(decLeftOperand)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        blnIsFirstDigit = True
+        btnDecimal.Enabled = True
+
+        If (decLeftOperand = Nothing And decRightOperand = Nothing) Then
+            decLeftOperand = Convert.ToDecimal(txtDisplay.Text)
+            strOperator = btnMultiply.Text
+            txtDisplay.Text = Convert.ToString(decLeftOperand)
+        Else
+            strLastOperator = strOperator
+            decRightOperand = Convert.ToDecimal(txtDisplay.Text)
+
+            If (decRightOperand = 0 And strLastOperator = "/") Then
+                txtDisplay.Text = "ERROR"
+                decRightOperand = Nothing
+                decLeftOperand = Nothing
+                strOperator = Nothing
+            Else
+                decLeftOperand = PerformCalculation(decLeftOperand, decRightOperand, strLastOperator)
+                decRightOperand = Nothing
+                strOperator = btnMultiply.Text
+                txtDisplay.Text = Convert.ToString(decLeftOperand)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        blnIsFirstDigit = True
+        btnDecimal.Enabled = True
+
+        If (decLeftOperand = Nothing And decRightOperand = Nothing) Then
+            decLeftOperand = Convert.ToDecimal(txtDisplay.Text)
+            strOperator = btnDivide.Text
+            txtDisplay.Text = Convert.ToString(decLeftOperand)
+        Else
+            strLastOperator = strOperator
+            decRightOperand = Convert.ToDecimal(txtDisplay.Text)
+
+            If (decRightOperand = 0 And strLastOperator = "/") Then
+                txtDisplay.Text = "ERROR"
+                decRightOperand = Nothing
+                decLeftOperand = Nothing
+                strOperator = Nothing
+            Else
+                decLeftOperand = PerformCalculation(decLeftOperand, decRightOperand, strLastOperator)
+                decRightOperand = Nothing
+                strOperator = btnDivide.Text
+                txtDisplay.Text = Convert.ToString(decLeftOperand)
+            End If
+        End If
+    End Sub
+
+    '****************************************************************************** 
+    'When the equal button is pressed the final calculation is performed and the
+    'result is displayed.  All values stored in memory are erased.  
+    '******************************************************************************
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+        decRightOperand = Convert.ToDecimal(txtDisplay.Text)
+        If (decRightOperand = 0 And strOperator = "/") Then
+            txtDisplay.Text = "ERROR"
+        ElseIf (decLeftOperand = Nothing) Then
+            decResult = Convert.ToDecimal(txtDisplay.Text)
+        Else
+            decResult = PerformCalculation(decLeftOperand, decRightOperand, strOperator)
+            txtDisplay.Text = Convert.ToString(decResult)
+        End If
+
+        decRightOperand = Nothing
+        decLeftOperand = Nothing
+        decResult = Nothing
+        strOperator = Nothing
+        blnIsFirstDigit = True
+
+        btnDecimal.Enabled = True
+    End Sub
+
+    '****************************************************************************** 
+    'Function which accepts two values and an operator and performs a function on 
+    'the two values and returns a value.
+    '******************************************************************************
+    Function PerformCalculation(ByVal decOperand1 As Decimal, ByVal decOperand2 As Decimal, ByVal strOperator As String) As Decimal
+        Dim decResult As Decimal
+
+        Select Case strOperator
+            Case "+"
+                decResult = decOperand1 + decOperand2
+            Case "-"
+                decResult = decOperand1 - decOperand2
+            Case "*"
+                decResult = decOperand1 * decOperand2
+            Case "/"
+                decResult = decOperand1 / decOperand2
+        End Select
+
+        Return decResult
+    End Function
+
+End Class
